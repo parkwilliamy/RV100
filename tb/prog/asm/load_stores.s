c9a82abb3a05d6aeb6b3aca9c0fb74bf9e74d4ac
parkwilliamy <parkwilliamy@gmail.com>
2025-11-26 00:18:07 -0500

    fix3

--- tb/prog/asm/load_stores.s
+++ tb/prog/asm/load_stores.s
@@ -3,9 +3,10 @@
     .section .text
 _start:
     
-    auipc x5, 0x5           # x5 = base address of .data section (0x5000)
+    auipc x5, 0x5              # x5 = base address of .data section (0x5000)
     li    x1, 2                # x1 = 2
-    addi  x0, x0, 0          # NOP
+    addi  x0, x0, 0            # NOP
+    addi  x0, x0, 0            # NOP
     sw    x1, 0(x5)            # mem[0x5000] = x1
     lw    x3, 0(x5)            # x3 = mem[0x5000]
     
