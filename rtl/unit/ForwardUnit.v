3d9362d43a397167541c2a2fed4d91845c13e818
William Park <parkwilliamy@gmail.com>
2025-11-30 12:38:15 -0500

    verified mem forwards, will write general forward/stall test now

--- rtl/unit/ForwardUnit.v
+++ rtl/unit/ForwardUnit.v
@@ -5,7 +5,7 @@ module ForwardUnit (
     input [1:0] MEM_RegSrc,
     input [4:0] EX_rs1, EX_rs2, MEM_rs2, MEM_rd, WB_rd,
     input [2:0] EX_ValidReg, MEM_ValidReg, WB_ValidReg,
-    input MEM_MemWrite, WB_MemRead,
+    input MEM_MemRead, MEM_MemWrite, WB_MemRead,
     output EX_rs1_fwd, EX_rs2_fwd, MEM_rs2_fwd,
     output reg [31:0] EX_rs1_fwd_data, EX_rs2_fwd_data, MEM_rs2_fwd_data
 );
@@ -13,11 +13,11 @@ module ForwardUnit (
     wire EX_rs1_MEM_fwd, EX_rs2_MEM_fwd, EX_rs1_WB_fwd, EX_rs2_WB_fwd;
     reg [31:0] MEM_rd_write_data;
 
-    assign EX_rs1_MEM_fwd = (EX_rs1 == MEM_rd) && (EX_ValidReg[1] && MEM_ValidReg[0]);
-    assign EX_rs2_MEM_fwd = (EX_rs2 == MEM_rd) && (EX_ValidReg[2] && MEM_ValidReg[0]);
+    assign EX_rs1_MEM_fwd = (EX_rs1 == MEM_rd) && (EX_ValidReg[1] && MEM_ValidReg[0]) && !MEM_MemRead;
+    assign EX_rs2_MEM_fwd = (EX_rs2 == MEM_rd) && (EX_ValidReg[2] && MEM_ValidReg[0]) && !MEM_MemRead;
     assign EX_rs1_WB_fwd = (EX_rs1 == WB_rd) && (EX_ValidReg[1] && WB_ValidReg[0]);
     assign EX_rs2_WB_fwd = (EX_rs2 == WB_rd) && (EX_ValidReg[2] && WB_ValidReg[0]);
-    assign MEM_rs2_WB_fwd = (MEM_rs2 == WB_rd && MEM_MemWrite && WB_MemRead) && (MEM_ValidReg[2] && WB_ValidReg[0]);
+    assign MEM_rs2_WB_fwd = (MEM_rs2 == WB_rd) && (MEM_MemWrite && WB_MemRead) && (MEM_ValidReg[2] && WB_ValidReg[0]);
     
     assign EX_rs1_fwd = (EX_rs1_MEM_fwd || EX_rs1_WB_fwd) && (EX_rs1 != 0);
     assign EX_rs2_fwd = (EX_rs2_MEM_fwd || EX_rs2_WB_fwd) && (EX_rs2 != 0);
