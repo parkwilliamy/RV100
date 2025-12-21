4a8ace70b6c36608d660f416e172bd1998ea181d
parkwilliamy <parkwilliamy@gmail.com>
2025-12-21 00:13:47 -0800

    added mem test and changed data type for loop alternating test

--- metric_tests/tests/mem1.c
+++ metric_tests/tests/mem1.c
@@ -1,26 +1,25 @@
 int main() {
 
-    int count = 0;
-    int flag = 0;
-    
-    for (int i = 0; i < 1000; i++) {
-        if (flag == 1) count++;
-        flag = !flag;
-    }
+    volatile unsigned char *p = (unsigned char*)0x6000;
+    p[0] = 1;
+    p[1] = 2;
+    p[2] = 3;
+    p[3] = 4;
+    while(1);
 
     int* CLK_CYCLE_ADDR = (int*)0x00004F00;
     int* INVALID_CLK_CYCLE_ADDR = (int*)0x00004F04;
     int* RETIRED_INSTRUCTIONS_ADDR = (int*)0x00004F08;
     int* CORRECT_PREDICTIONS_ADDR = (int*)0x00004F0C;
     int* TOTAL_PREDICTIONS_ADDR = (int*)0x00004F10;
-    int* RESULT_ADDR = (int*)0x00006000;
+    
 
     *CLK_CYCLE_ADDR = 0;
     *INVALID_CLK_CYCLE_ADDR = 0;
     *RETIRED_INSTRUCTIONS_ADDR = 0;
     *CORRECT_PREDICTIONS_ADDR = 0;
     *TOTAL_PREDICTIONS_ADDR = 0;
-    *RESULT_ADDR = count;
+    
 
     while(1);
 
