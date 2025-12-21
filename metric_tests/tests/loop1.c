5bdefd64791502e22e4ae9d4a1fd13c8d842a65b
parkwilliamy <parkwilliamy@gmail.com>
2025-12-21 12:33:18 -0800

    made addr pointers volatile

--- metric_tests/tests/loop1.c
+++ metric_tests/tests/loop1.c
@@ -6,12 +6,12 @@ int main() {
         count++;
     }
 
-    int* CLK_CYCLE_ADDR = (int*)0x00004F00;
-    int* INVALID_CLK_CYCLE_ADDR = (int*)0x00004F04;
-    int* RETIRED_INSTRUCTIONS_ADDR = (int*)0x00004F08;
-    int* CORRECT_PREDICTIONS_ADDR = (int*)0x00004F0C;
-    int* TOTAL_PREDICTIONS_ADDR = (int*)0x00004F10;
-    int* RESULT_ADDR = (int*)0x00006000;
+    volatile int* CLK_CYCLE_ADDR = (volatile int*)0x00004F00;
+    volatile int* INVALID_CLK_CYCLE_ADDR = (volatile int*)0x00004F04;
+    volatile int* RETIRED_INSTRUCTIONS_ADDR = (volatile int*)0x00004F08;
+    volatile int* CORRECT_PREDICTIONS_ADDR = (volatile int*)0x00004F0C;
+    volatile int* TOTAL_PREDICTIONS_ADDR = (volatile int*)0x00004F10;
+    volatile int* RESULT_ADDR = (volatile int*)0x00006000;
 
     *CLK_CYCLE_ADDR = 0;
     *INVALID_CLK_CYCLE_ADDR = 0;
--- metric_tests/tests/mem1.c
+++ metric_tests/tests/mem1.c
@@ -1,18 +1,17 @@
 int main() {
 
-    volatile unsigned char *p = (unsigned char*)0x6000;
-    p[0] = 1;
-    p[1] = 2;
-    p[2] = 3;
-    p[3] = 4;
+    volatile int* CLK_CYCLE_ADDR = (volatile int*)0x00004F00;
+    volatile int* INVALID_CLK_CYCLE_ADDR = (volatile int*)0x00004F04;
+    volatile int* RETIRED_INSTRUCTIONS_ADDR = (volatile int*)0x00004F08;
+    volatile int* CORRECT_PREDICTIONS_ADDR = (volatile int*)0x00004F0C;
+    volatile int* TOTAL_PREDICTIONS_ADDR = (volatile int*)0x00004F10;
+    volatile int* RESULT_ADDR = (volatile int*)0x00006000;
 
-    int* CLK_CYCLE_ADDR = (int*)0x00004F00;
-    int* INVALID_CLK_CYCLE_ADDR = (int*)0x00004F04;
-    int* RETIRED_INSTRUCTIONS_ADDR = (int*)0x00004F08;
-    int* CORRECT_PREDICTIONS_ADDR = (int*)0x00004F0C;
-    int* TOTAL_PREDICTIONS_ADDR = (int*)0x00004F10;
+    RESULT_ADDR[0] = 1;
+    RESULT_ADDR[1] = 2;
+    RESULT_ADDR[2] = 3;
+    RESULT_ADDR[3] = 4;
     
-
     *CLK_CYCLE_ADDR = 0;
     *INVALID_CLK_CYCLE_ADDR = 0;
     *RETIRED_INSTRUCTIONS_ADDR = 0;
