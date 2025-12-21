27e0699233cbbfa756169dab0b465b0b315a6cbd
parkwilliamy <parkwilliamy@gmail.com>
2025-12-21 15:19:25 -0800

    mem works 2?

--- metric_tests/tests/mem1.c
+++ metric_tests/tests/mem1.c
@@ -1,16 +1,17 @@
 int main() {
 
-    volatile int* CLK_CYCLE_ADDR = (int*)0x00004F00;
-    volatile int* INVALID_CLK_CYCLE_ADDR = (int*)0x00004F04;
-    volatile int* RETIRED_INSTRUCTIONS_ADDR = (int*)0x00004F08;
-    volatile int* CORRECT_PREDICTIONS_ADDR = (int*)0x00004F0C;
-    volatile int* TOTAL_PREDICTIONS_ADDR = (int*)0x00004F10;
     volatile unsigned char* RESULT_ADDR = (unsigned char*)0x6000;
 
     RESULT_ADDR[0] = 1;
     RESULT_ADDR[1] = 2;
     RESULT_ADDR[2] = 3;
     RESULT_ADDR[3] = 4;
+
+    volatile int* CLK_CYCLE_ADDR = (int*)0x00004F00;
+    volatile int* INVALID_CLK_CYCLE_ADDR = (int*)0x00004F04;
+    volatile int* RETIRED_INSTRUCTIONS_ADDR = (int*)0x00004F08;
+    volatile int* CORRECT_PREDICTIONS_ADDR = (int*)0x00004F0C;
+    volatile int* TOTAL_PREDICTIONS_ADDR = (int*)0x00004F10;
     
     *CLK_CYCLE_ADDR = 0;
     *INVALID_CLK_CYCLE_ADDR = 0;
