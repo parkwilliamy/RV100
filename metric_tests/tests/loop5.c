63b9343047200fd0b49ecdea28117972e20d00b6
parkwilliamy <parkwilliamy@gmail.com>
2025-12-21 22:53:15 -0800

    done with tests???

--- metric_tests/tests/loop5.c
+++ metric_tests/tests/loop5.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
-static inline uint32_t xorshift32(uint32_t *s) {
+static inline uint32_t xorshift32(uint32_t* s) {
     uint32_t x = *s;
     x ^= x << 13; x ^= x >> 17; x ^= x << 5;
     *s = x;
@@ -8,10 +8,10 @@ static inline uint32_t xorshift32(uint32_t *s) {
 }
 
 int main() {
-    volatile uint32_t *OUT = (uint32_t*)0x6000;
+    volatile uint32_t* OUT = (uint32_t*)0x6000;
 
     uint32_t seed = 0x12345678;
-    uint32_t iters = 1000;
+    uint32_t iters = 100000;
     uint32_t a_taken = 0, b_taken = 0;
 
     for (uint32_t i = 0; i < iters; i++) {
@@ -21,10 +21,8 @@ int main() {
         if (a) b_taken++;        // Branch B (perfectly correlated)
     }
 
-    OUT[0] = a_taken;
-    OUT[1] = b_taken;
+    *OUT = b_taken;
 
-    /*
     volatile int* CLK_CYCLE_ADDR = (volatile int*)0x00005000;
     volatile int* INVALID_CLK_CYCLE_ADDR = (volatile int*)0x00005004;
     volatile int* RETIRED_INSTRUCTIONS_ADDR = (volatile int*)0x00005008;
@@ -36,6 +34,6 @@ int main() {
     *RETIRED_INSTRUCTIONS_ADDR = 0;
     *CORRECT_PREDICTIONS_ADDR = 0;
     *TOTAL_PREDICTIONS_ADDR = 0;
-    */
+    
     while (1);
 }
