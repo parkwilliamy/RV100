5b7886623bd1c5cc5e5230e299e1831579f78c90
parkwilliamy <parkwilliamy@gmail.com>
2025-12-21 18:49:15 -0800

    removed metrics for now

--- metric_tests/tests/loop2.c
+++ metric_tests/tests/loop2.c
@@ -8,18 +8,21 @@ int main() {
         flag = !flag;
     }
 
+    /*
     volatile int* CLK_CYCLE_ADDR = (volatile int*)0x00007000;
     volatile int* INVALID_CLK_CYCLE_ADDR = (volatile int*)0x00007004;
     volatile int* RETIRED_INSTRUCTIONS_ADDR = (volatile int*)0x00007008;
     volatile int* CORRECT_PREDICTIONS_ADDR = (volatile int*)0x0000700C;
     volatile int* TOTAL_PREDICTIONS_ADDR = (volatile int*)0x00007014;
+    */
     volatile int* RESULT_ADDR = (volatile int*)0x00006000;
-
+    /*
     *CLK_CYCLE_ADDR = 0;
     *INVALID_CLK_CYCLE_ADDR = 0;
     *RETIRED_INSTRUCTIONS_ADDR = 0;
     *CORRECT_PREDICTIONS_ADDR = 0;
     *TOTAL_PREDICTIONS_ADDR = 0;
+    */
     *RESULT_ADDR = count;
 
     while(1);
