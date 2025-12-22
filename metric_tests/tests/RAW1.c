956e01d7d9acfe3566060b4895a7c82100f6dec5
parkwilliamy <parkwilliamy@gmail.com>
2025-12-21 17:04:21 -0800

    testing reads again

--- metric_tests/tests/RAW1.c
+++ metric_tests/tests/RAW1.c
@@ -1,3 +1,5 @@
+/*
+
 int main() {
 
     //volatile int a,b,c,d,e,f,g,h;
@@ -24,7 +26,35 @@ int main() {
     RESULT_ADDR[1] = d;
     RESULT_ADDR[2] = f;
     RESULT_ADDR[3] = h;
-    */
+    
+    while(1);
+
+}
+
+*/
+
+int main() {
+
+    volatile unsigned char* RESULT_ADDR = (volatile unsigned char*)0x6000;
+
+    RESULT_ADDR[0] = 1;
+    RESULT_ADDR[1] = 2;
+    RESULT_ADDR[2] = 3;
+    RESULT_ADDR[3] = 4;
+
+    volatile int* CLK_CYCLE_ADDR = (volatile int*)0x00007000;
+    volatile int* INVALID_CLK_CYCLE_ADDR = (volatile int*)0x00007004;
+    volatile int* RETIRED_INSTRUCTIONS_ADDR = (volatile int*)0x00007008;
+    volatile int* CORRECT_PREDICTIONS_ADDR = (volatile int*)0x0000700C;
+    volatile int* TOTAL_PREDICTIONS_ADDR = (volatile int*)0x00007014;
+    
+    *CLK_CYCLE_ADDR = 0;
+    *INVALID_CLK_CYCLE_ADDR = 0;
+    *RETIRED_INSTRUCTIONS_ADDR = 0;
+    *CORRECT_PREDICTIONS_ADDR = 0;
+    *TOTAL_PREDICTIONS_ADDR = 0;
+    
+
     while(1);
 
 }
